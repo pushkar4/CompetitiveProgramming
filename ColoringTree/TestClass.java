428f3ff48b3ec0a336ab3baaec9f1c0b72428f57
Pushkar Sharma <pushkar1291@gmail.com>
2022-09-30 06:01:40 +0530

    Reformatted indentation

--- ColoringTree/TestClass.java
+++ ColoringTree/TestClass.java
@@ -1,41 +1,41 @@
 
 class TestClass2 {
-	
-	public static int N,Q,t,v,A[],col=1;
 
-	public static void main(String[] args) {
-		
-		FastReader sc = new FastReader();
-		
-		N = sc.nextInt();
-		Q = sc.nextInt();
-		A = new int[N+1];
-		
-		while(Q != 0) {
-			Q--;
-			
-			t = sc.nextInt();
-			v = sc.nextInt();
-			
-			if(t==1) {
-				// print color of v
-				System.out.println(A[v]);
-			} else {
-				// color all the vertex of the subtree rooted with vertex v
-				// with color which is numbered 1 more than the color used in 
-				// the previous query of this type.
-				
-				color(v);
-				col++;
-			}
-		}
-	}
+  public static int N, Q, t, v, A[], col = 1;
 
-	private static void color(int node) {
-		if(node <= N) {
-			A[node] = col;
-			color(2*node);
-			color(2*node + 1);
-		}
-	}
+  public static void main(String[] args) {
+
+    FastReader sc = new FastReader();
+
+    N = sc.nextInt();
+    Q = sc.nextInt();
+    A = new int[N + 1];
+
+    while (Q != 0) {
+      Q--;
+
+      t = sc.nextInt();
+      v = sc.nextInt();
+
+      if (t == 1) {
+        // print color of v
+        System.out.println(A[v]);
+      } else {
+        // color all the vertex of the subtree rooted with vertex v
+        // with color which is numbered 1 more than the color used in
+        // the previous query of this type.
+
+        color(v);
+        col++;
+      }
+    }
+  }
+
+  private static void color(int node) {
+    if (node <= N) {
+      A[node] = col;
+      color(2 * node);
+      color(2 * node + 1);
+    }
+  }
 }
